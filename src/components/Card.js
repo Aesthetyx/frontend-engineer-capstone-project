b62cc5e6051201fba5a1dfd7a8ab48c29a8ee797
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-27 18:38:54 +0000

    Wed, Jun 28, 2023, 2:38 AM +08:00

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+// Package imports
+import {
+    Flex,
+    Spacer,
+    Image,
+    VStack,
+    Heading,
+    Text,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+
+// Component imports
+
+// Asset imports
+
+// CSS imports
+import "./Card.css";
+
+export default function Card(props) {
+    return (
+        
+    )
+}
--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -10,8 +10,38 @@ import {
     Button,
   } from "@chakra-ui/react";
 
+// Component imports
+import Card from "./Card"
+
 // Asset imports
-import breadDish from "../icons_assets/restauranfood.jpg";
+import greekSaladPic from "../icons_assets/greek\ salad.jpg";
+import bruchettaPic from "../icons_assets/bruchetta.png";
+import lemonDessertPic from "../icons_assets/lemon\ dessert.jpg";
 
 // CSS imports
 import "./Highlights.css";
+
+export default function Highlights() {
+    const thisWeeksSpecials = [
+        {
+            name: "Greek salad",
+            price: "$12.99",
+            image: greekSaladPic,
+            description: "
+
+
+        },
+    ]
+    return (
+        <Flex>
+            <VStack>
+                <HStack></HStack>
+                <HStack>
+                    <Card food={greekSalad} />
+                    <Card food={bruchetta}/>
+                    <Card food={lemonDessert} />
+                </HStack>
+            </VStack>
+        </Flex>
+    )
+}
