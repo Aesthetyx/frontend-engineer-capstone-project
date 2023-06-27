7e75d464f34e3955f508de75d893aba0529ebc6b
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 02:28:07 +0800

    test commit

---
+++ src/components/Highlights.js
@@ -0,0 +1,17 @@
+// Package imports
+import {
+    Flex,
+    Spacer,
+    Image,
+    VStack,
+    Heading,
+    Text,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+
+// Asset imports
+import breadDish from "../icons_assets/restauranfood.jpg";
+
+// CSS imports
+import "./Highlights.css";
