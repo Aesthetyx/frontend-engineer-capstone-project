19d1ae303d9ecd3b58ac00c8f01df43ca044143e
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 07:56:31 +0000

    Wed, Jun 28, 2023, 3:56 PM +08:00

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -16,17 +16,15 @@ import "./Rating.css";
 
 export default function Rating(props) {
     return (
-        <Flex borderRadius="lg" bg="#EDEFEE" w="200px" h="300px">
-            <VStack>
-                <Flex>{props.info.stars} stars</Flex>
-                <Flex align="start">
-                    <Image src={props.info.image} alt={props.info.name} boxSize="64px"/>
-                    <Text>{props.info.name}</Text>
-                </Flex>
-                <Box>
-                    <Text>{props.info.review}</Text>
-                </Box>
-            </VStack>
+        <Flex borderRadius="lg" bg="#EDEFEE" w="200px" h="300px" direction="column">
+            <Flex>{props.info.stars} stars</Flex>
+            <Flex align="start">
+                <Image src={props.info.image} alt={props.info.name} boxSize="64px"/>
+                <Text>{props.info.name}</Text>
+            </Flex>
+            <Box>
+                <Text>{props.info.review}</Text>
+            </Box>
         </Flex>
     )
 }
