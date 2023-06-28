530b760b714a83e038ea4b2ac86cb43f5b70a2f8
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 07:54:15 +0000

    Wed, Jun 28, 2023, 3:54 PM +08:00

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,22 +21,20 @@ import "./Card.css";
 
 export default function Card(props) {
     return (
-        <Box borderRadius="lg" w="260px" bg="lightgrey" h="500px">
-            <VStack>
-                <Image src={props.dish.image} alt={props.dish.name} w="100%" maxH="160px" objectFit="cover"/>
-                <Flex>
-                    <Heading as="h2">{props.dish.name}</Heading>
-                    <Spacer />
-                    <Heading as="h2" color="orange">{props.dish.price}</Heading>
-                </Flex>
-                <Box w="90%" h="160px">
-                    <Text>{props.dish.description}</Text>
-                </Box>
-                <HStack>
-                    <Text>Order a delivery</Text>
-                    <Image src={dishIcon}/>
-                </HStack>
-            </VStack>
-        </Box>
+        <Flex borderRadius="lg" w="260px" bg="lightgrey" h="450px" direction="column">
+            <Image src={props.dish.image} alt={props.dish.name} w="100%" maxH="160px" objectFit="cover"/>
+            <Flex>
+                <Heading as="h2">{props.dish.name}</Heading>
+                <Spacer />
+                <Heading as="h2" color="orange">{props.dish.price}</Heading>
+            </Flex>
+            <Box w="90%" h="160px">
+                <Text>{props.dish.description}</Text>
+            </Box>
+            <HStack>
+                <Text>Order a delivery</Text>
+                <Image src={dishIcon}/>
+            </HStack>
+        </Flex>
     )
 }
