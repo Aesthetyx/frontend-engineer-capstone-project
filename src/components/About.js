65bca4790c40c94e8786e5abc98914d8a4d08a7e
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 06:40:36 +0000

    Wed, Jun 28, 2023, 2:40 PM +08:00

--- src/components/About.js
+++ src/components/About.js
@@ -19,28 +19,30 @@ import marioAndAdrianB from "../icons_assets/Mario\ and\ Adrian\ b.jpg";
 import "./About.css";
 
 export default function About(){
-    <Flex>
-        <HStack>
-            <Spacer />
-            <VStack>
-                <Box alignItems="start">
-                    <Heading as="h1" color="#f4ce14">
-                        Little Lemon
-                    </Heading>
-                    <Heading as="h2" color="#ffffff">
-                        Chicago
-                    </Heading>
-                    <Box flex="1" maxW="200px">
-                        <Text color="#ffffff">
-                            We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist
-                        </Text>
+    return (
+        <Flex>
+            <HStack>
+                <Spacer />
+                <VStack>
+                    <Box alignItems="start">
+                        <Heading as="h1" color="#f4ce14">
+                            Little Lemon
+                        </Heading>
+                        <Heading as="h2" color="#ffffff">
+                            Chicago
+                        </Heading>
+                        <Box flex="1" maxW="200px">
+                            <Text color="#ffffff">
+                                We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist
+                            </Text>
+                        </Box>
                     </Box>
-                </Box>
-            </VStack>
-            <Spacer />
-            <Image src={marioAndAdrianA} alt="Mario and Adrian A" />
-            <Image src={marioAndAdrianB} alt="Mario and Adrian B" />
-            <Spacer />
-        </HStack>
-    </Flex>
+                </VStack>
+                <Spacer />
+                <Image src={marioAndAdrianA} alt="Mario and Adrian A" />
+                <Image src={marioAndAdrianB} alt="Mario and Adrian B" />
+                <Spacer />
+            </HStack>
+        </Flex>
+    )
 }
