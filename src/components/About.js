677dde27e0ba7b53f990cf99ca0a9f3d69cef26f
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 06:28:33 +0000

    Wed, Jun 28, 2023, 2:28 PM +08:00

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+// Package imports
+import {
+    Flex,
+    Spacer,
+    Image,
+    VStack,
+    HStack,
+    Heading,
+    Text,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+
+// Asset imports
+import randomUser1 from "../icons_assets/randomUser1.jpeg";
+import randomUser2 from "../icons_assets/randomUser2.jpeg";
+
+// CSS imports
+import "./Testimonials.css";
