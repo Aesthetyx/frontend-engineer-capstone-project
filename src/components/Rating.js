140bf23c7d8c97d559e2d115e7b396360067d728
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 06:08:19 +0000

    Wed, Jun 28, 2023, 2:08 PM +08:00

---
+++ src/components/Rating.js
@@ -0,0 +1,35 @@
+// Package imports
+import {
+    Flex,
+    Spacer,
+    Image,
+    VStack,
+    HStack,
+    Heading,
+    Text,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+
+// CSS imports
+import "./Rating.css";
+
+export default function Rating(props) {
+    return (
+        <Box borderRadius="lg">
+            <VStack>
+                <Image src={props.dish.image} alt={props.dish.name}/>
+                <HStack>
+                    <Heading as="h2">{props.dish.name}</Heading>
+                    <Spacer />
+                    <Heading as="h2">{props.dish.price}</Heading>
+                </HStack>
+                <Text>{props.dish.description}</Text>
+                <HStack>
+                    <Text>Order a delivery</Text>
+                    <Image src={dishIcon}/>
+                </HStack>
+            </VStack>
+        </Box>
+    )
+}
