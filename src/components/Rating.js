1e386af91f3bf366cdf18e83fb1e961b38a0bb06
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 08:10:24 +0000

    Wed, Jun 28, 2023, 4:10 PM +08:00

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -9,6 +9,7 @@ import {
     Text,
     Box,
     Button,
+    StarIcon,
   } from "@chakra-ui/react";
 
 // CSS imports
@@ -17,7 +18,9 @@ import "./Rating.css";
 export default function Rating(props) {
     return (
         <Flex borderRadius="lg" bg="#EDEFEE" w="200px" h="300px" direction="column">
-            <Flex>{props.info.stars} stars</Flex>
+            <Flex>
+                {Array(5).fill("").map((_, index)->(<StarIcon key={index} color={index < props.info.stars? "yellow" : "grey"}/>))}
+            </Flex>
             <Flex align="start">
                 <Image src={props.info.image} alt={props.info.name} boxSize="80px"/>
                 <Text>{props.info.name}</Text>
