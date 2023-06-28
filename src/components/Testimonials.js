be3dc05bf73a02925cb3c98da6eb1847435499bc
Aesthetyx <106989358+Aesthetyx@users.noreply.github.com>
2023-06-28 05:48:54 +0000

    Wed, Jun 28, 2023, 1:48 PM +08:00

---
+++ src/components/Testimonials.js
@@ -0,0 +1,23 @@
+// Package imports
+import {
+    Flex,
+    Spacer,
+    Image,
+    VStack,
+    HStack,
+    Heading,
+    Text,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+
+// Component imports
+import Rating from "./Rating"
+
+// Asset imports
+import greekSaladPic from "../icons_assets/greek\ salad.jpg";
+import bruchettaPic from "../icons_assets/bruchetta.png";
+import lemonDessertPic from "../icons_assets/lemon\ dessert.jpg";
+
+// CSS imports
+import "./Highlights.css";
